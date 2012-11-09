32e57eb9d959ba05b4947dff9389c3e1978acd3e
Alex Moriarty <alexander@dal.ca>
2012-11-08 23:15:43 -0400

    a6 initial commit. Done?

---
+++ js/a6scripts.js
@@ -0,0 +1,39 @@
+function setLocation(local){
+	$.cookie('myLocal',local)
+	if(local=="tl"){
+		$("#selector").removeClass()
+		$("#selector").addClass("tl")
+	}else if(local=="tr"){
+		$("#selector").removeClass()
+		$("#selector").addClass("tr")
+
+	}else if(local=="bl"){
+		$("#selector").removeClass()
+		$("#selector").addClass("bl")
+
+	}else if(local=="br"){
+		$("#selector").removeClass()
+		$("#selector").addClass("br")
+	}else{
+		$("#selector").removeClass()
+		$("#selector").addClass("df")
+		$.cookie('myLocal','df')
+	}
+}
+
+function changeLocation(){
+	var foo = document.getElementById("locationSelector").selectedIndex;
+	var local = document.getElementById("locationSelector").options[foo].value;
+
+	setLocation(local);	
+}
+
+function hideSelector(){
+	$("#selector").toggle();
+}
+$(document).ready(function () {
+	cVal = $.cookie('myLocal');
+	if (cVal != ''){
+		setLcation(cVal);
+	}
+});
