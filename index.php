f38466dfebddfe26e41df590898e176702225466
Alex Moriarty <alexander@dal.ca>
2012-11-26 00:28:28 -0400

    added a7 forced

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--
+INFX1606 - Starter kit for Assignment 1.
+-->
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Alexander Moriarty" >
+    <meta name="description" content="Assignment 1: intro to website creation. Become familiar with uploading files to webpage.">
+    <title>Intro to website creation</title>
+    <link rel="stylesheet" type="text/css" href="./css/basic.css"/ media="screen">
+    <link rel="stylesheet" type="text/css" href="./css/print.css"/ media="print">
+    <script type="text/javascript" src="./js/scripts.js"></script>
+</head>
+<body>
+	<div id="container">
+	<header>
+		<div id="banner">
+		<h1>
+		Intro to website creation
+		</h1>
+		</div>
+		<div id="topnav">
+		<nav>
+		<a href="/~moriarty">Home</a>
+		<a href="./a1/index.html">1. Plain</a>
+		<a href="./a2/index.html">2. Bonn</a>
+		<a href="./a3/index.html">3. Robots</a>
+		<a href="./a4/index.html">4. Version Control</a>
+		</nav>
+		</div>
+	</header>
+	<div id="menu">
+	<ul>
+	</ul>
+	</div>
+	<div id="content">
+	<?php echo '<p>Test</p>'?>
+	</div>
+	<div id="subcontent">
+
+	</div>
+	<footer>
+		<div id="contact">
+		<address>
+		Alexander Moriarty <br />
+		Dalhousie University <br />
+		Halifax, NS, Canada <br />
+		moriarty AT cs.dal.ca<br />
+		</address>
+		</div>
+		<div id="footnav">
+		<nav>
+		<a href="/~moriarty">Home</a> |
+		</nav>
+		</div>
+	<div class="clear"></div>
+	</footer>
+	</div>
+    </body>
+</html>
